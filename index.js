fa710b623b83a20cbc0756c607dc600765db641a
Eric Foard <foard265@gmail.com>
2017-02-18 14:16:35 -0800

    search function complete + test scaffold

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Node {
   constructor(key, value) {
     this.key = key;
@@ -30,11 +32,35 @@ class Trie {
     }
     return value;
   }
+
+  search(str) {
+    if (typeof str !== 'string') {
+      return new Error(`search key must be a string: ${str}`);
+    }
+    let currNode = this.root;
+    let char, child;
+
+    for (let i=0; i<str.length; i++) {
+      char = str.charAt(i);
+      child = currNode.children[char];
+      if (!child) {
+        return false;
+      }
+      currNode = child;
+    }
+    return currNode.value;
+  }
 }
 
 let t = new Trie();
 console.log(t);
-console.log(t.insert('wow', 7));
+t.insert('wow', 7);
+t.insert('whw', 3);
+t.insert('zgb', 1);
 console.log(JSON.stringify(t.root, null, 2));
+console.log('searching...', t.search('wow'));
+console.log('searching...', t.search('hey'));
+console.log('searching...', t.search('whw'));
+console.log('searching...', t.search('w'));
 
 module.exports = Trie;
--- test.js
+++ test.js
@@ -7,10 +7,6 @@ describe('when constructing a new trie', () => {
 });
 
 describe('when inserting elements into the trie', () => {
-  // what are the different cases with insertion?
-    // insert a blank string
-    // insert a word that already exists (replace value?)
-    // insert a new string
   it('should throw an error if insert string is falsy', () => {
 
   });
@@ -18,6 +14,24 @@ describe('when inserting elements into the trie', () => {
 
   });
   it('should correctly insert a new string', () => {
+
+  });
+});
+
+describe('when searching for elements in the trie', () => {
+  it('should throw an error if search target is not a string', () => {
+
+  });
+  it('should return null if search string is empty string', () => {
+
+  });
+  it('should return false if search target does not exist in tree', () => {
+
+  });
+  it('should return value at if target found & target includes leaf node', () => {
+
+  });
+  it('should return null at if target found & target DOES NOT include leaf node', () => {
 
   });
 });
